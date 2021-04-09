ceb6577e4c7f9d0e0258d3f463178e0947347e2a
Francois Foltete <ffoltete@unistra.fr>
2021-04-09 12:17:38 +0200

    single file components

---
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'MenueItem',
+    props: ['addToShoppingCart','image','name','inStock','quantity']
+}
+</script>
+
+<template>
+    <div >
+        <img class="menu-item__image" :src="image.source" :alt="image.alt"
+        />
+        <div>
+            <h3> {{ name }} </h3>
+            <p v-if="inStock">In Stock</p>
+            <p v-else>Out of Stock</p>
+            <div>
+                <label for="add-item-quantity"> Quantity: {{ quantity }} </label>
+                <input
+                    v-model.number="quantity"
+                    id="add-item-quantity"
+                    type="number"
+                />
+                <button @click="addToShoppingCart">
+                    Add to Shopping Cart
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style></style>
